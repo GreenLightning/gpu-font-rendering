f183f497aa18b38714b634172d7b9034d7e7c52c
Green Lightning <GreenLightning.git@googlemail.com>
2022-07-20 14:31:27 +0200

    Add mouse movement

---
+++ source/glm.hpp
@@ -0,0 +1,18 @@
+#pragma once
+
+#define GLM_FORCE_RADIANS
+#define GLM_ENABLE_EXPERIMENTAL
+
+#include <glm/vec2.hpp>
+#include <glm/vec3.hpp>
+#include <glm/mat4x4.hpp>
+#include <glm/trigonometric.hpp>
+
+#include <glm/gtc/constants.hpp>
+#include <glm/gtc/matrix_access.hpp>
+#include <glm/gtc/matrix_transform.hpp>
+#include <glm/gtc/type_ptr.hpp>
+
+#include <glm/gtx/hash.hpp>
+#include <glm/gtx/string_cast.hpp>
+#include <glm/gtx/transform.hpp>
--- source/main.cpp
+++ source/main.cpp
@@ -1,4 +1,6 @@
+#include <cmath>
 #include <iostream>
+#include <limits>
 #include <string>
 
 #include <defer.hpp>
@@ -9,15 +11,194 @@
 #include <ft2build.h>
 #include FT_FREETYPE_H
 
+#include "glm.hpp"
+
 #include "shader_catalog.hpp"
+
+struct Transform {
+	float fovy         = glm::radians(60.0f);
+	float distance     = 0.42f;
+	glm::mat3 rotation = glm::mat3(1.0f);
+	glm::vec3 position = glm::vec3(0.0f);
+
+	glm::mat4 getProjectionMatrix(float aspect) {
+		return glm::perspective(/* fovy = */ glm::radians(60.0f), aspect, 0.002f, 12.000f);
+	}
+
+	glm::mat4 getViewMatrix() {
+		auto translation = glm::translate(position);
+		return glm::lookAt(glm::vec3(0, 0, distance), glm::vec3(0, 0, 0), glm::vec3(0, 1, 0)) * glm::mat4(rotation) * translation;
+	}
+};
+
+struct DragController {
+	Transform* transform = nullptr;
+	int activeButton = -1;
+
+	double dragX, dragY;
+	double wrapX, wrapY;
+	double virtualX, virtualY;
+	glm::vec3 dragTarget;
+
+	bool unprojectMousePositionToXYPlane(GLFWwindow* window, double x, double y, glm::vec3& result) {
+		int iwidth = 0, iheight = 0;
+		glfwGetWindowSize(window, &iwidth, &iheight);
+
+		double width = iwidth;
+		double height = iheight;
+
+		glm::mat4 projection = transform->getProjectionMatrix(float(width / height));
+		glm::mat4 view = transform->getViewMatrix();
+
+		double relX = x/width*2.0 - 1.0;
+		double relY = y/height*2.0 - 1.0;
+
+		glm::vec4 clipPos = glm::vec4(float(relX), -float(relY), 0.5f, 1.0f);
+		glm::vec4 worldPos = glm::inverse(projection * view) * clipPos;
+		worldPos *= 1.0f / worldPos.w;
+
+		glm::vec3 pos = glm::vec3(glm::column(glm::inverse(view), 3));
+		glm::vec3 dir = glm::normalize(glm::vec3(worldPos) - pos);
+		float t = -pos.z / dir.z;
+
+		result = pos + t * dir;
+		return t > 0.0f;
+	}
+
+	void onMouseButton(GLFWwindow* window, int button, int action, int mods) {
+		if (action == GLFW_PRESS && activeButton == -1) {
+			activeButton = button;
+			glfwGetCursorPos(window, &dragX, &dragY);
+			wrapX = std::numeric_limits<double>::quiet_NaN();
+			wrapY = std::numeric_limits<double>::quiet_NaN();
+			virtualX = dragX;
+			virtualY = dragY;
+
+			glm::vec3 target;
+			bool ok = unprojectMousePositionToXYPlane(window, dragX, dragY, target);
+			dragTarget = ok ? target : glm::vec3();
+		} else if (action == GLFW_RELEASE && activeButton == button) {
+			activeButton = -1;
+			dragX = 0.0;
+			dragY = 0.0;
+			wrapX = std::numeric_limits<double>::quiet_NaN();
+			wrapY = std::numeric_limits<double>::quiet_NaN();
+			virtualX = 0.0;
+			virtualY = 0.0;
+			dragTarget = glm::vec3();
+		}
+	}
+
+	void onCursorPos(GLFWwindow* window, double x, double y) {
+		if (activeButton == -1) return;
+
+		int iwidth = 0, iheight = 0;
+		glfwGetWindowSize(window, &iwidth, &iheight);
+
+		double width = iwidth;
+		double height = iheight;
+
+		double deltaX = x-dragX;
+		double deltaY = y-dragY;
+
+		if (!std::isnan(wrapX) && !std::isnan(wrapY)) {
+			double wrapDeltaX = x-wrapX;
+			double wrapDeltaY = y-wrapY;
+			if (wrapDeltaX*wrapDeltaX+wrapDeltaY*wrapDeltaY < deltaX*deltaX+deltaY*deltaY) {
+				deltaX = wrapDeltaX;
+				deltaY = wrapDeltaY;
+				wrapX = std::numeric_limits<double>::quiet_NaN();
+				wrapY = std::numeric_limits<double>::quiet_NaN();
+			}
+		}
+
+		dragX = x;
+		dragY = y;
+
+		double targetX = x;
+		double targetY = y;
+		bool changed = false;
+		if (targetX < 0) {
+			targetX += width - 1;
+			changed = true;
+		} else if (targetX >= width) {
+			targetX -= width - 1;
+			changed = true;
+		}
+		if (targetY < 0) {
+			targetY += height - 1;
+			changed = true;
+		} else if (targetY >= height) {
+			targetY -= height - 1;
+			changed = true;
+		}
+		if (changed) {
+			glfwSetCursorPos(window, targetX, targetY);
+			wrapX = targetX;
+			wrapY = targetY;
+		}
+
+		if (activeButton == GLFW_MOUSE_BUTTON_2) {
+			virtualX += deltaX;
+			virtualY += deltaY;
+
+			glm::vec3 target;
+			bool ok = unprojectMousePositionToXYPlane(window, virtualX, virtualY, target);
+			if (ok) {
+				float x = transform->position.x;
+				float y = transform->position.y;
+				glm::vec3 delta = target - dragTarget;
+				transform->position.x = glm::clamp(x + delta.x, -2.0f, 2.0f);
+				transform->position.y = glm::clamp(y + delta.y, -2.0f, 2.0f);
+			}
+		} else if (activeButton == GLFW_MOUSE_BUTTON_3) {
+			// Turntable rotation.
+			double size = glm::min(width, height);
+			glm::mat3 rx = glm::rotate(float(deltaX / size * glm::pi<double>()), glm::vec3(0, 0, 1));
+			glm::mat3 ry = glm::rotate(float(deltaY / size * glm::pi<double>()), glm::vec3(1, 0, 0));
+			transform->rotation = ry * transform->rotation * rx;
+		} else {
+			// Trackball rotation.
+			double size = glm::min(width, height);
+			glm::mat3 rx = glm::rotate(float(deltaX / size * glm::pi<double>()), glm::vec3(0, 1, 0));
+			glm::mat3 ry = glm::rotate(float(deltaY / size * glm::pi<double>()), glm::vec3(1, 0, 0));
+			transform->rotation = ry * rx * transform->rotation;
+		}
+	}
+
+	void onScroll(GLFWwindow* window, double xOffset, double yOffset) {
+		float factor = glm::clamp(1.0-float(yOffset)/10.0, 0.1, 1.9);
+		transform->distance = glm::clamp(transform->distance * factor, 0.010f, 10.000f);
+	}
+};
 
 namespace {
 	FT_Library library;
+
+	Transform transform;
+	DragController dragController;
+
+	// Empty VAO used when the vertex shader has no input and only uses gl_VertexID,
+	// because OpenGL still requires a non-zero VAO to be bound for the draw call.
+	GLuint emptyVAO;
 
 	GLuint quadVAO;
 
 	std::unique_ptr<ShaderCatalog> shaderCatalog;
 	std::shared_ptr<ShaderCatalog::Entry> backgroundShader;
+	std::shared_ptr<ShaderCatalog::Entry> basicShader;
+}
+
+static void mouseButtonCallback(GLFWwindow* window, int button, int action, int mods) {
+	dragController.onMouseButton(window, button, action, mods);
+}
+
+static void cursorPosCallback(GLFWwindow* window, double x, double y) {
+	dragController.onCursorPos(window, x, y);
+}
+
+static void scrollCallback(GLFWwindow* window, double xOffset, double yOffset) {
+	dragController.onScroll(window, xOffset, yOffset);
 }
 
 int main(int argc, char* argv[]) {
@@ -56,10 +237,51 @@ int main(int argc, char* argv[]) {
 		}
 	}
 
+	dragController.transform = &transform;
+	glfwSetMouseButtonCallback(window, mouseButtonCallback);
+	glfwSetCursorPosCallback(window, cursorPosCallback);
+	glfwSetScrollCallback(window, scrollCallback);
+
+	glGenVertexArrays(1, &emptyVAO);
+
 	glGenVertexArrays(1, &quadVAO);
+	{
+		glBindVertexArray(quadVAO);
+
+		GLuint vbo, ebo;
+		glGenBuffers(1, &vbo);
+		glGenBuffers(1, &ebo);
+
+		glm::vec3 vertices[] = {
+			glm::vec3(0, 0, 0),
+			glm::vec3(0.2f, 0, 0),
+			glm::vec3(0.2f, 0.2f, 0),
+			glm::vec3(0, 0.2f, 0),
+		};
+
+		GLuint indices[] = {
+			0, 1, 2,
+			0, 2, 3,
+		};
+
+		glBindBuffer(GL_ARRAY_BUFFER, vbo);
+		glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+
+		glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, ebo);
+		glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
+
+		glEnableVertexAttribArray(0);
+		glVertexAttribPointer(0, 3, GL_FLOAT, false, 0, 0);
+
+		glBindVertexArray(0);
+
+		glBindBuffer(GL_ARRAY_BUFFER, 0);
+		glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, 0);
+	}
 
 	shaderCatalog = std::make_unique<ShaderCatalog>("shaders");
 	backgroundShader = shaderCatalog->get("background");
+	basicShader = shaderCatalog->get("basic");
 
 	while(!glfwWindowShouldClose(window)) {
 		shaderCatalog->update();
@@ -73,11 +295,34 @@ int main(int argc, char* argv[]) {
 		glClearColor(0.0f, 0.0f, 0.0f, 1.0f);
 		glClear(GL_COLOR_BUFFER_BIT);
 
-		GLuint program = backgroundShader->getProgram();
-		glUseProgram(program);
-		glBindVertexArray(quadVAO);
+		GLuint program, location;
 
+		glm::mat4 projection = transform.getProjectionMatrix((float)width / height);
+		glm::mat4 view = transform.getViewMatrix();
+		glm::mat4 model = glm::mat4(1.0f);
+
+		program = backgroundShader->getProgram();
+		glUseProgram(program);
+		glBindVertexArray(emptyVAO);
 		glDrawArrays(GL_TRIANGLE_STRIP, 0, 4);
+		glBindVertexArray(0);
+		glUseProgram(0);
+
+		program = basicShader->getProgram();
+		glUseProgram(program);
+
+		location = glGetUniformLocation(program, "projection");
+		glUniformMatrix4fv(location, 1, false, glm::value_ptr(projection));
+		location = glGetUniformLocation(program, "view");
+		glUniformMatrix4fv(location, 1, false, glm::value_ptr(view));
+		location = glGetUniformLocation(program, "model");
+		glUniformMatrix4fv(location, 1, false, glm::value_ptr(model));
+
+		location = glGetUniformLocation(program, "color");
+		glUniform4f(location, 0.8f, 0.8f, 0.8f, 1.0f);
+
+		glBindVertexArray(quadVAO);
+		glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
 		glBindVertexArray(0);
 		glUseProgram(0);
 
